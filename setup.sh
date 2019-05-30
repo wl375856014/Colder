5c35bbbb161a3c8649fa3936f901dcf4e03ffae1
王力 <375856014@qq.com>
2019-05-30 11:09:42 +0800

    --

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
-# cd WebTest
+# cd webtest
 git pull --rebase origin master;
-docker stop WebTest;
-docker rm WebTest;
-docker build -t WebTest:$image_version .;
+docker stop webtest;
+docker rm webtest;
+docker build -t webtest:$image_version .;
 docker images;
-docker run -p 10001:80 -d --name WebTest WebTest:$image_version;
+docker run -p 10001:80 -d --name webtest webtest:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs WebTest;
+docker logs webtest;
 #??build????????    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
